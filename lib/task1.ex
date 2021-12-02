f40cde71559ebe48da367b0baf256319447cc8b7
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2021-12-02 12:16:26 +0200

    Modify task1 to process the data in a function

--- lib/task1.ex
+++ lib/task1.ex
@@ -1,18 +1,22 @@
 defmodule AOC.Task1 do
+  @moduledoc "https://adventofcode.com/2021/day/1"
   @data_file Path.join(__DIR__, "data/task1_input.txt")
-  @data File.read!(@data_file)
-        |> String.split("\n")
-        |> Enum.map(&String.to_integer/1)
 
   def run_a do
-    Stream.chunk_every(@data, 2, 1, :discard)
+    Stream.chunk_every(data(), 2, 1, :discard)
     |> Enum.count(fn [prev, next] -> next > prev end)
   end
 
   def run_b do
-    Stream.chunk_every(@data, 3, 1, :discard)
+    Stream.chunk_every(data(), 3, 1, :discard)
     |> Stream.map(&Enum.sum/1)
     |> Stream.chunk_every(2, 1, :discard)
     |> Enum.count(fn [prev, next] -> next > prev end)
+  end
+
+  defp data do
+    File.read!(@data_file)
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
   end
 end
